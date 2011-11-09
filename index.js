8299928b8a11292084ff32db73663b62116476cf
John Schulz <JFSIII@JFSIII.org>
2011-11-09 08:29:53 -0500

    Whitespace cleanup (tabs, trailing spaces, etc)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
     // Use mustache.js templating to create layout
-    
+
     var imageArray = { images: [
         {"file": "3.jpg"},
-        {"file": "4.jpg"},  
-        {"file": "5.jpg"}, 
+        {"file": "4.jpg"},
+        {"file": "5.jpg"},
         {"file": "logo1.png"},
-        {"file": "icon1.png", "colorCount": "4", "class": "fbIcon"}  
+        {"file": "icon1.png", "colorCount": "4", "class": "fbIcon"}
     ]};
 
     var html = Mustache.to_html($('#template').html(), imageArray);
@@ -16,11 +16,11 @@ $(document).ready(function () {
     // Use lettering.js to give letter by letter styling control for the h1 title
     $("h1").lettering();
 
-    
+
     // Once images are loaded, loop through each one, getting dominant color
     // and palette and displaying them.
     $('img').imagesLoaded(function () {
-        
+
         $('img').each(function (index) {
 
             var imageSection = $(this).closest('.imageSection'),
@@ -28,9 +28,9 @@ $(document).ready(function () {
 
             // Dominant Color
             var dominantColor = getDominantColor(this);
-            
+
             swatchEl = $('<div>', {
-	        'class': 'swatch'
+                'class': 'swatch'
             }).css('background-color','rgba('+dominantColor.r+','+dominantColor.g+ ','+dominantColor.b+', 1)');
             imageSection.find('.dominantColor .swatches').append(swatchEl);
 
@@ -42,10 +42,10 @@ $(document).ready(function () {
 
             var medianCutPalette = imageSection.find('.medianCutPalette .swatches');
             $.each(medianPalette, function (index, value) {
-	        swatchEl = $('<div>', {
-	            'class': 'swatch'
-	        }).css('background-color','rgba('+value[0]+','+value[1]+ ','+value[2]+', 1)');
-	        medianCutPalette.append(swatchEl);
+                swatchEl = $('<div>', {
+                    'class': 'swatch'
+                }).css('background-color','rgba('+value[0]+','+value[1]+ ','+value[2]+', 1)');
+                medianCutPalette.append(swatchEl);
             });
 
         });
