1976bc405c9624c623d74a6069043a4dba1afca9
John Schulz <JFSIII@JFSIII.org>
2011-11-10 08:35:29 -0500

    Consolidate color swatch addition, because 5c02cff standardized the color format.

--- index.js
+++ index.js
@@ -17,30 +17,29 @@ $(document).ready(function () {
     $("h1").lettering();
 
 
-    // For each image: 
+    // For each image:
     // Once image is loaded, get dominant color and palette and display them.
     $('img').bind('load', function (event) {
         var image = event.target;
         var $image = $(image);
-        var imageSection = $image.closest('.imageSection'),
-            swatchEl;
+        var imageSection = $image.closest('.imageSection');
+        var appendColors = function (colors, root) {
+            $.each(colors, function (index, value) {
+                var swatchEl = $('<div>', {'class': 'swatch'})
+                    .css('background-color', 'rgba('+ value +', 1)');
+                root.append(swatchEl);
+            });
+        };
 
         // Dominant Color
         var dominantColor = getDominantColor(image);
-
-        swatchEl = $('<div>', {'class': 'swatch' })
-            .css('background-color','rgba('+ dominantColor +', 1)');
-        imageSection.find('.dominantColor .swatches').append(swatchEl);
+        var dominantSwatch = imageSection.find('.dominantColor .swatches');
+        appendColors([dominantColor], dominantSwatch);
 
         // Palette
         var colorCount = $image.attr('data-colorcount') ? $image.data('colorcount') : 10;
         var medianPalette = createPalette(image, colorCount);
         var medianCutPalette = imageSection.find('.medianCutPalette .swatches');
-
-        $.each(medianPalette, function (index, value) {
-            swatchEl = $('<div>', {'class': 'swatch'})
-                .css('background-color', 'rgba('+ value +', 1)');
-            medianCutPalette.append(swatchEl);
-        });
+        appendColors(medianPalette, medianCutPalette);
     });
 });
