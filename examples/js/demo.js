2ffa707ae0bdc2ebdad4701e6839ec204e12a8b5
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2019-04-28 22:10:32 -0700

    Cleanup: remove unused jquery, tabs to spaces

--- examples/js/demo.js
+++ examples/js/demo.js
@@ -1,6 +1,34 @@
+var colorThief = new ColorThief();
+
+var images = [
+    'black.png',
+    'red.png',
+    'rainbow-horizontal.png',
+    'rainbow-vertical.png',
+    // 'transparent.png',
+    // 'white.png',
+];
+
+// Render example images
+var examplesHTML = Mustache.to_html(document.getElementById('image-tpl').innerHTML, images);
+document.getElementById('example-images').innerHTML = examplesHTML;
+
+// Once images are loaded, process them
+document.querySelectorAll('.image').forEach((image) => {
+    const section = image.closest('.image-section');
+    if (this.complete) {
+        showColorsForImage(image, section);
+    } else {
+        image.addEventListener('load', function() {
+            showColorsForImage(image, section);
+        });
+    }
+})
+
 // Run Color Thief functions and display results below image.
 // We also log execution time of functions for display.
 const showColorsForImage = function(image, section) {
+    const file = section.src;
     const start = Date.now();
     const color = colorThief.getColor(image);
     const elapsedTimeForGetColor = Date.now() - start;
@@ -20,29 +48,3 @@ const showColorsForImage = function(image, section) {
     const outputEl = section.querySelector('.output');
     outputEl.innerHTML = ouputHTML;
 };
-
-var colorThief = new ColorThief();
-
-var images = [
-    'black.png',
-    'red.png',
-    'rainbow-horizontal.png',
-    'rainbow-vertical.png',
-    // 'transparent.png',
-    // 'white.png',
-];
-
-// Render example images
-var examplesHTML = Mustache.to_html(document.getElementById('image-tpl').innerHTML, images);
-document.getElementById('example-images').innerHTML = examplesHTML;
-
-document.querySelectorAll('.image').forEach((image) => {
-    const section = image.closest('.image-section');
-    if (this.complete) {
-        showColorsForImage(image, section);
-    } else {
-        image.addEventListener('load', function() {
-            showColorsForImage(image, section);
-        });
-    }
-})
