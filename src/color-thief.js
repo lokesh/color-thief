b00090f1a8f87c7c6e29ff72097861e20b30eed3
Cheng Wang <1688764+wangcheng678@users.noreply.github.com>
2019-12-12 22:45:50 +0800

    use image naturalHeightnaturalHeight

--- src/color-thief.js
+++ src/color-thief.js
@@ -31,8 +31,8 @@ import core from './core.js';
 const CanvasImage = function (image) {
     this.canvas  = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
-    this.width  = this.canvas.width  = image.width;
-    this.height = this.canvas.height = image.height;
+    this.width  = this.canvas.width  = image.naturalWidth;
+    this.height = this.canvas.height = image.naturalHeight;
     this.context.drawImage(image, 0, 0, this.width, this.height);
 };
 
